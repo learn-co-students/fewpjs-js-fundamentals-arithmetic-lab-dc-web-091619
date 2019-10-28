8054300d3dcb7ae0a13d5698cbef3c0da9beb686
kh45 <khaledhassan45@gmail.com>
2019-10-28 08:56:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+
+const multiply = num1 * num2;
+
+const random = (Math.random() * 10) + 1;
+
+const mod = 40 % 36;
+
+const max = Math.max(1,2,3,20);
